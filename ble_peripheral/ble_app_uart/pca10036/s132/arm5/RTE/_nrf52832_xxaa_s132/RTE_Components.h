87876c871f463aa42e27e6f69cb34ae5dd6b68e4
Kosta Konstas <kokonstaS2gmail.com>
2020-11-13 01:57:19 +0000

    nit

---
+++ ble_peripheral/ble_app_uart/pca10036/s132/arm5/RTE/_nrf52832_xxaa_s132/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_uart_s132_pca10036' 
+ * Target:  'nrf52832_xxaa_s132' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define NRF_LOG_USES_UART=1
+#define SWI_DISABLE0
+
+#endif /* RTE_COMPONENTS_H */
